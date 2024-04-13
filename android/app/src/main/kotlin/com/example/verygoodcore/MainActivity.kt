658e6915a4cf12b32ebaf9db33d5ac1ad7f63303
chendjou237 <chenxhenor@gmail.com>
2024-04-13 16:18:23 +0200

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.chococam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:chococam/app/app.dart';
+import 'package:chococam/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
